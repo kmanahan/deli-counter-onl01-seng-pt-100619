d5c9a04b2139322e30aea8dee5ecb6a5d874f27c
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-11-05 21:12:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,14 @@
 # Write your code 
 def line(katz_deli)
+<<<<<<< HEAD
   str = "The line is currently:"
   if katz_deli.length >= 1
     katz_deli.each_with_index {|name, index| str.concat(" #{index + 1}. " "#{name}")}
+=======
+  str = "The line is currently: "
+  if katz_deli.length >= 1
+    katz_deli.each_with_index {|name, index| str.concat("#{index + 1}.", "#{name}", " ")} 
+>>>>>>> aa0c9e8ecd7f74b40c5b9cfc195c3d8a4861a164
     puts str
   else 
   puts "The line is currently empty."
@@ -14,6 +20,7 @@ def take_a_number(katz_deli, name)
     puts "Welcome, " + "#{name}" +"." +" You are number "+ "#{katz_deli.length}" + " in line." 
 end
 
+<<<<<<< HEAD
 def now_serving(katz_deli)
 if katz_deli.length >= 1 
   puts "Currently serving #{katz_deli.shift}." 
@@ -22,3 +29,6 @@ puts "There is nobody waiting to be served!"
 end
 end
 
+=======
+  
+>>>>>>> aa0c9e8ecd7f74b40c5b9cfc195c3d8a4861a164
